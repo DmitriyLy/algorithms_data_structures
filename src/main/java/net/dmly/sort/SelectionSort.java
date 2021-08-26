a992eda7454aab6fdd21cdc212e9ac85fab0ebd5
DmitriyLy <dmitriy.lisay@gmail.com>
2021-08-26 21:59:13 +0300

    selection sort 1

--- src/main/java/net/dmly/sort/SelectionSort.java
+++ src/main/java/net/dmly/sort/SelectionSort.java
@@ -6,6 +6,7 @@ public class SelectionSort {
     public static void main(String[] args) {
 
         Integer[] array = {6, 2, 4, 9, 3 ,7};
+        System.out.println(String.format("Initial array: %s", Arrays.toString(array)));
 
         Integer[] sorted1 = selectionSort1(array);
         System.out.println(String.format("sorted1: %s", Arrays.toString(sorted1)));
@@ -14,12 +15,18 @@ public class SelectionSort {
 
     private static Integer[] selectionSort1(Integer[] input) {
         Integer[] source = Arrays.copyOf(input, input.length);
-
-        int minIndex = 0;
-        int sortedIndex = 0;
-
+        int minIndex;
         for (int i = 0; i < source.length; i++) {
+            minIndex = i;
+            for (int j = i; j < source.length; j++) {
+                if (source[j] < source[minIndex]) {
+                    minIndex = j;
+                }
+            }
 
+            Integer buffer = source[i];
+            source[i] = source[minIndex];
+            source[minIndex] = buffer;
         }
 
         return source;
